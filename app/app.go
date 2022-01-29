4c5685f9cca7ddaba134c22bd4faf059d3ec58db
Juan David Diaz <juan.diaz@doublevpartners.com>
2022-01-29 13:37:57 -0500

    Router Initialization

--- app/app.go
+++ app/app.go
@@ -1,1 +1,24 @@
 package app
+
+import "github.com/gorilla/mux"
+
+// App struct
+type App struct {
+	Router *mux.Router
+}
+
+// Initialize App
+func New() *App {
+	app := &App{
+		Router: mux.NewRouter(),
+	}
+
+	app.InitializeRouter()
+
+	return app
+}
+
+// Initialize Router
+func (a *App) InitializeRouter() {
+	a.Router.HandleFunc("/", nil).Methods("GET")
+}
--- main.go
+++ main.go
@@ -1,1 +1,8 @@
 package main
+
+import "movieapi/app"
+
+// Main function to start the application
+func main(){
+	app.New()
+}
