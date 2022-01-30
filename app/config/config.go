d434981e5b7cc2a1ef61b0cd5fc75fc853a2c6dc
Juan David Diaz <juan.diaz@doublevpartners.com>
2022-01-30 12:09:04 -0500

    Remove dateformat

--- app/config/config.go
+++ app/config/config.go
@@ -7,6 +7,5 @@ var (
 	DbName             string = "movie"
 	MgConnectionString string = DbPrefix + DbHost + ":" + DbPort
 	MovieCollection    string = "movies"
-	DateFormat         string = "20060102150405"
 	OmbdApiKey         string = "40c55e7c"
 )
